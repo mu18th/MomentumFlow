fad65981409267a4ed6a8147e6db57e5ffcb94a9
Muath Alyoubi <mkalyoubi18@gmail.com>
2025-09-02 15:32:39 +0300

    Day 3 Week 2: Flask routing and templates practice

--- app.py
+++ app.py
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/tasks")
+def tasks():
+    tasks = ["x", "y", "z"]
+    return render_template("tasks.html", tasks=tasks)
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
