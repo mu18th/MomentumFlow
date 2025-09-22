c09b2c4a0d7b838262a5e2a12f14e08041e6bd67
Muath Alyoubi <mkalyoubi18@gmail.com>
2025-09-22 22:06:37 +0300

    day 2 in Week 5: done

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-import sqlite3
 from flask import Flask, g, render_template, request, redirect, jsonify, make_response
 from db import get_db, close_db
-
+from werkzeug.security import check_password_hash, generate_password_hash
 from kanbanAI import generate_subtasks
 from helpers import apology
 
+# Configure application and database
 app = Flask(__name__)
 app.config["DATABASE"] = "instance/smart_kanban.db"
 
-#db related
+
+#db related routes
 with app.app_context():
     db = get_db()
 
@@ -18,11 +19,15 @@ with app.app_context():
 def teardown_appcontext_db(exception):
     close_db()
 
+
+# index route
 @app.route("/")
 def index():
     tasks = db.execute("SELECT * FROM tasks WHERE user_id = ?", (1,)).fetchall()
     return render_template("index.html", tasks=tasks)
 
+
+#not important routes
 @app.route("/about")
 def about():
     
@@ -33,6 +38,8 @@ def tasks():
     tasks = db.execute("SELECT * FROM tasks WHERE user_id = ? ORDERED BY priority", (1,))
     return render_template("tasks.html", tasks=tasks)
 
+
+#add task route
 @app.route("/addtask",  methods=["GET", "POST"])
 def addtask():
     if request.method == "POST":
@@ -61,40 +68,7 @@ def addtask():
         return render_template("addtask.html")
 
 
-"""@app.route("/update-status",  methods=["GET", "POST"])
-def updateTask():
-    if request.method == "POST":
-        id = request.form.get("taskID")
-    
-        if not id:
-            return apology("must provide ID", 400)
-        
-        status = request.form.get("status")
-        if not status:
-            return apology("must spacify status", 400)
-        
-        db.execute("UPDATE tasks SET status = ? WHERE id = ?", (status, id))
-        db.commit()
-        
-        return redirect("/") 
-    else:
-        return render_template("update.html")"""
-    
-"""@app.route("/delete-task",  methods=["GET", "POST"])
-def deleteTask():
-    if request.method == "POST":
-        id = request.form.get("taskID")
-
-        if not id:
-            return apology("must provide ID", 400)
-    
-        db.execute("DELETE FROM tasks WHERE id = ?", (id,))
-        db.commit()
-
-        return redirect("/")
-    else:
-        return render_template("delete.html")"""
-
+#to delete drag and drop
 @app.route("/delete-task",  methods=["POST"])
 def deleteTask():
     task = request.get_json()
@@ -153,11 +127,13 @@ def generateSubtasks():
     
     return jsonify({"taskID": id, "message": "updated"}), 200
 
+
+#register and logen routes
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        user_name = request.form.get("username")
-        if not user_name:
+        username = request.form.get("username")
+        if not username:
             return apology("must provide username", 400)
         
         email = request.form.get("email")
@@ -175,9 +151,17 @@ def register():
         if password != confirmation:
             return apology("passwords do not match", 400)
         
+        try:
+            db.execute("INSERT INTO users (username, email, hash) VALUES (?, ?, ?)", 
+                    (username, email, generate_password_hash(password)))
+            db.commit()
+        except:
+            return apology("username or email already exists", 400)
+
         return redirect("/")
     else:
         return render_template("register.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
