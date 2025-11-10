effdbb3c1adc0dad80e87dd9266fb43783787b25
Muath Alyoubi <mkalyoubi18@gmail.com>
2025-11-11 01:34:30 +0300

    update logo

--- db.py
+++ db.py
@@ -3,7 +3,7 @@
 import sqlite3
 from flask import g
 
-DATABASE = "instance/MomentumFlowAI.db"
+DATABASE = "instance/MomentumFlow.db"
 
 # ORDER is a constante indicate the oreder of returned quary
 ORDER =  """ORDER BY
