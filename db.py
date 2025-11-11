1ce3ef8c011e91598cf09835dfaf5945e6b3af09
Muath Alyoubi <mkalyoubi18@gmail.com>
2025-11-12 02:12:34 +0300

    Prepare for Render deployment

--- db.py
+++ db.py
@@ -8,7 +8,7 @@ import sqlite3
 # Third-party imports
 from flask import g
 
-DATABASE = os.getenv("DATABASE_URL", "MomentumFlow.db")
+DATABASE = os.getenv("/var/data/MomentumFlow.db")
 
 # ORDER is a constante indicate the oreder of returned quary
 ORDER =  """ORDER BY
