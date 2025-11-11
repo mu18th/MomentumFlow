6789f6fad92bd9f585dc05a22addda7cd5a64ed6
Muath Alyoubi <mkalyoubi18@gmail.com>
2025-11-12 01:44:27 +0300

    Prepare for Render deployment

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ from helpers import apology, login_required, get_date_deatails
 
 # Configure application
 app = Flask(__name__)
+app.teardown_appcontext(close_db)
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-app.teardown_appcontext(close_db())
 
 """ board routes """
 
