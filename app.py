da19b01a1ea19f14ec3301b17e89dcf8bbf14463
Muath Alyoubi <mkalyoubi18@gmail.com>
2025-11-12 01:38:08 +0300

    Prepare for Render deployment

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-app.teardown_appcontext(close_db)
+app.teardown_appcontext(close_db())
 
 """ board routes """
 
